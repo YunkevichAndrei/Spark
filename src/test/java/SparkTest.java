ec1fc5bb6351faecb0bf4586b18502b1480805fc
Yunkevich Andrei <andreyha.by@gmail.com>
2017-12-03 17:38:35 +0300

    change

--- src/test/java/SparkTest.java
+++ src/test/java/SparkTest.java
@@ -16,14 +16,10 @@ public class SparkTest {
     @Test
     public void countsWord() throws Exception {
         final File file = new File("data/input/file.txt");
-        final File textFile = new File("data/input/text.txt");
 
         FileWorker.createRandomMatrix(3, 3);
         final BlockMatrix result = MatrixService.createBlockMatrixFromFile(sc, file.getAbsolutePath());
-       /* final List<Tuple2<Integer, String>> map = WordCount.counts(sc(), words.getAbsolutePath());
-        for (Tuple2<Integer, String> i : map)
-            System.out.println(i.toString());
-*/
+
         FileWorker.outputMatrix(result);
     }
 
